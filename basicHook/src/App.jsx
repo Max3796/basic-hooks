dd0ad657c3b2fbaa8884da57b91dc80aff56e383
Max3796 <gurpreet.singh@kalvium.community>
2024-04-17 12:30:14 +0530

    Done

--- basicHook/src/App.jsx
+++ basicHook/src/App.jsx
@@ -1,21 +1,30 @@
 import './App.css'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Counter1 from './components/Counter1'
 import Counter2 from './components/Counter2'
 import Counter3 from './components/Counter3'
 import Counter4 from './components/Counter4'
 import Effect from './components/Effects/Effect'
+import UseContext from './components/UseContext'
+
+export const ToggleTheme = React.createContext()
 
 function App() {
   const [show,setShow]=useState(true)
+  const [state,setState] = useState(true)
+
+  const handleToggle = ()=>{
+    setState(state=>!state)
+  }
   return (
-    <>
+    <ToggleTheme.Provider value={state}>
        <button onClick={()=>{
       setShow(!show)
     }}>
       C H A N G E
     </button>
-
+    <button onClick={handleToggle}>Toggle</button>
+      <UseContext/>
     {!show ? (
       <>
     <Counter4/>
@@ -26,7 +35,7 @@ function App() {
     ):( <Effect/>)
    
     }
-    </>
+    </ToggleTheme.Provider>
   )
 }
 
